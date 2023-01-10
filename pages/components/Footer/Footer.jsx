5a83d147167330433eea6e6c3e8302dac03b7428
Gerardo Castillo <gerrcass@gmail.com>
2023-01-09 22:17:54 -0400

    missing anchor _blank target without rel='noopener noreferrer'

--- pages/components/Footer/Footer.jsx
+++ pages/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="pb-2 mx-auto max-w-[375px] desktop:max-w-[66rem] text-[11px] text-center text-attribution">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>.
-            Coded by <a href="https://gerardocastillo.me/" target="_blank">@gerrcass</a>.
+            Coded by <a href="https://gerardocastillo.me/" target="_blank" rel="noopener noreferrer">@gerrcass</a>.
         </footer>
     )
 }
